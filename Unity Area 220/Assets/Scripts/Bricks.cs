408ba81d6865bf0dabfdb4775c1f79784f481ec5
Peet <peetvipz@gmail.com>
2020-07-28 15:14:25 +1200

    Area 220 Current

---
+++ Unity Area 220/Assets/Scripts/Bricks.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bricks : MonoBehaviour
+{
+    public GameObject brickParticle;
+    public int Hitpoints;
+    private bool Destroyed;
+
+    void OnCollisionEnter(Collision other)
+    {
+        ReduceHitpoints();
+
+
+
+    }
+
+    private void ReduceHitpoints()
+    {
+        Hitpoints--;
+        if (Hitpoints <= 0)
+        {
+
+            DestroyBrick();
+        }
+    }
+
+    public void DestroyBrick()
+    {
+        Instantiate(brickParticle, transform.position, Quaternion.identity);
+
+        if (!Destroyed)
+        {
+            GM.instance.OnDestroyBrick();
+            Destroy(gameObject);
+            Destroyed = true;
+
+        }
+    }
+}
